c21949591106093e5b69040fde67785be5d56ce9
dorisgjata <dorisgjata@oakland.edu>
2020-06-04 01:05:20 -0400

    Added more views and functionality

--- src/app.js
+++ src/app.js
@@ -8,7 +8,24 @@
 
 // You can require definitions from other JavaScript and JSON files.
 // Your code will be bundled together using WebPack during DFF compilation.
-var terms = require("./terms.json");
+
+
+let terms = require("./terms.json");
+
+let time = new Date();
+let flightTime = new Date(time);
+flightTime.setHours(flightTime.getHours()+3);
+flightTime.setMinutes(flightTime.getMinutes()+23);
+const flightInfo = {
+  flightTime: flightTime,
+  flightNo: 'abcd',
+  coords: {
+    latitude: 44.583038,
+    longitude: -93.133987
+  },
+  address: "DTW"
+};
+
 // Load each term into built-in flow context for Terms & Conditions.
 ngi.cards("_ngi:terms", terms);
 
@@ -31,74 +48,174 @@ ngi.flow('flightFlow', {
       }],
       links: {
         neighbors: ['flightTrack']
-      }, 
-
+      },
     })
     .addRoute(
     'flightTrack', {
       layout: 'Form',
       links: {
-        neighbors: ['flightTime']
+        neighbors: ['flightTime','flightMain']
       }, 
-
     })
     .addRoute(
       'flightTime',{
-          layout: 'Grid',
-          options: {
-            showContent: true,
-            borderless: true
+          layout: 'Detail',
+          actions: [{
+            label: 'Flight',
+            action: function() {
+              //recordVoice();
+            }
           },
-    }
+        ],
+      links: {
+        neighbors: ['flightTime','navMap']
+      },    
+    })
+    .addRoute(
+      'navMap', {
+        layout: 'MapDetail',
+      }      
   );
-  const clock = () => {
-    let time = new Date();
-    return `${time.getHours()}: ${time.getMinutes()}`;
-  };  
-  const interval = () => {
-     setInterval(clock, 1000)
-  };
- 
-  interval();
- let currPosition = ngi.vehicle.getPosition()
-  .then((coords) => {
-    console.log(coords);
-  });
-  let destination = ngi.vehicle.setDestination(coordinates)
+  
+  const getCountdown = () => {
+    let countdown = flightTime.getTime() - time.getTime(); //time in milliseconds 
+    let timer = setInterval( () => {
+      if (countdown > 0) {
+        countdown -= 60000;
+        //calculate hours and min from milliseconds
+        let hr = Math.floor((countdown % 86400000) / 3600000);
+        let min = Math.floor((countdown % 3600000) / 60000);
+        let minLabel =  min < 10?  `0${min} m` : `${min} m`;
 
+        ngi.state.set('hr', `${hr} hr`);
+        ngi.state.set('min', minLabel);
+    
+       } else {
+          clearInterval(timer)
+       }
+      }, 60000);//update every minute not every second for less updates
+    };
+
+    getCountdown();
+
+  const recordVoice = () => {
+    let sayFlight = '';
+    
+    let recordingSession = gm.voice.startSpeechRecSession(beginRecording, 'If you want to head to the airport to catch your flight say flight');
+    const beginRecording = () => {
+      let recordingConfig = {
+        intro: 'If you want to head to the airport to catch your flight  say flight after the tone',
+        silenceDetection: true,
+        silenceLength: 1000,
+        maxRecordingWindow: 30000,
+        noiseSuppression: 0
+      }
+      gm.voice.startRecording(finishedRecording, recordingConfig);
+    }
+  
+    const finishedRecording = (file) => {
+      console.log('File saved to:', file);
+      gm.voice.stopSpeechRecSession(recordingSession);
+    }  
+    //need to convert to txt
+    //if sayFlight === 'flight' 
+    // navigate to the airport  
+
+  }
+
+   const onSuccess = () => {
+    console.log("Successful!");
+  }
+
+  const onFailure = () => {
+      console.log("An error occured.");
+  }
+
+  // function getPosition(position){
+  //      if (position !== null || position !== undefined ) {
+  //         console.log(position.coords.latitude, position.coords.longitude);
+  //        let currentLat = position.coords.latitude;
+  //        let currentLong = position.coords.longitude;
+  //        // ngi.state.set('currentLat', position.coords.latitude);
+  //         //ngi.state.set('currentLong', position.coords.longitude);
+  //       }
+       
+  //   };
+    
+  // //watchPosition
+ //  gm.info.getCurrentPosition(onSuccess, onFailure, getPosition, true);
+
+
+ gm.nav.setDestination(onSuccess, onFailure, flightInfo.coords, true);
+  // function tripTime(){
+  //   let speed = gm.info.watchVehicleData(getSpeed, onSuccess, onFailure, ['average_speed']);  
+  // }
+
+  // function getSpeed(vehicleData){
+  //   let speed = vehicleData.speed;
+  // };
+ 
+  // function flightAlert(currTime, timeToDest, flightTime) {
+  //   let message = 'Leave now to catch flight in time!';
+  //   if ((flightTime - currTime - 1 ) <= (timeToDest)) {
+  //     /**I am calculating the time remaining catch the flight and the time to travel to the airport.
+  //        It is optimal to arrive in the arport between 1-2 hours for check-in and boarding
+  //        so I am allocating that time as well.  
+  //     */
+  //     message = 'You will not make your flight!'
+     
+  //   }
+  //   return ngi.toast.load({
+  //       title: message,
+  //       timeout: -1,
+  //       actions: [
+  //         {
+  //           label: 'Dismiss',
+  //           action: function() { 
+  //             this.route('flightTime');
+  //           }
+  //         }
+  //       ]
+  //     });    
+  // } 
 
   ngi.cards("flightFlow.flightMain", {
     title: 'Welcome to Flight Tracker!',
-
-    body: `<div> Current time: </>
-           <h1> ${clock()} </h1>
-            Would you like to track a flight?`, 
+    body: 'This app can track your upcoming flight and help you navigate to the airport. Would you like to track a flight?'
   });
-  
+
   ngi.form("flightFlow.flightTrack", {
+    title: 'Welcome to Flight Tracker!',
     onSubmit: function(flightNumber) {
       console.log("Form value", flightNumber);
       ngi.state.set('flightNumber', flightNumber);
       this.route('flightTime');
     },
     onCancel: function() {
-      this.back('flightMain');
+      this.route('flightMain');
     },
     fields: [
       {
         type: "input",
-        name: "Flight Number",
-        label: "Fligt no.",
+        name: "flightNumber",
+        label: "Flight Number",
         maxLength: 10
       },
     ]
   });
-  ngi.cards('flightFlow.flightTime', [
-    { title: 'Departure time', body: '10:00 pm'},
-    { title: 'Time to airport', body: '1 hr'},
-    { title: 'Position', body: info},
 
-  ]);
+  ngi.cards('flightFlow.flightTime', {
+     title: 'Flight departure countdown',
+     body:`<h1>${ngi.state.get('hr')} : ${ngi.state.get('min')}</h1>`,
+     refresh: 60000
+    },
+  );
+
+  ngi.cards('flightFlow.navMap',{
+    title: 'Map',
+    //location: 
+  });
+  
 // Specify your own entry flow, this will connect to Splash, Terms, and About.
 ngi.init('flightFlow');
 
